869b014f57ffd62e35fbac9626a58369ab04313a
Tyrali <38586806+Tyrali@users.noreply.github.com>
2018-04-21 16:27:04 +0100

    Basic
    Basic movement code

---
+++ New Unity Project/Assets/new.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+var distanceRight = 1;
+var startPosition : Vector2;
+var limit = 10;
+
+
+function Start () { //Starting frame
+		startPosition = this.gameObject.transform.position;
+	 	this.gameObject.transform.position.y += 10;
+}
+
+function Update () {//Reoccuring frame
+ 	this.gameObject.transform.position.x += distanceRight;
+ 	if(this.gameObject.transform.position.x - startPosition.x > limit){
+ 	this.gameObject.transform.position = startPosition;
+ 	}
+}
+
